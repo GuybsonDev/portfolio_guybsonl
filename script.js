9848bc20b67be64db72b3369280f758f4919f60d
Guybson Lopes <guybsonlopescomercial@gmail.com>
2025-01-21 18:19:24 -0300

    Inicializando principais arquivos para o projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+// Exemplo de mensagem ao enviar o formulário
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+    alert('Mensagem enviada com sucesso!');
+  });
+  
