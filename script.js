e799c383707f5f0706c1a3a116c3ef84c93a5c16
Guybson Lopes <guybsonlopescomercial@gmail.com>
2025-01-21 18:29:11 -0300

    Realizando mudanças visuais

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// Exemplo de mensagem ao enviar o formulário
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
-    alert('Mensagem enviada com sucesso!');
+// Detecta quando as seções entram na tela
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('.scroll-section');
+    
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const sectionVisible = window.innerHeight / 1.5; // A seção será visível antes de entrar completamente
+  
+      if (sectionTop < sectionVisible) {
+        section.classList.add('visible');
+      } else {
+        section.classList.remove('visible');
+      }
+    });
   });
   
