fca6ca1b05261a4e286e6b2f60c59bc35176eb3a
GuybsonDev <guybsonlopescomercial@gmail.com>
2025-03-30 11:40:12 -0300

    Adicionado tela de contato, faltando apenas a logo do e-mail. Projeto quase finalizado, restando apenas responsividade para tablets e telas ultrawides.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -4,7 +4,7 @@ import { useRef } from "react";
 
 function Contact() {
   const ref = useRef(null);
-  const isInView = useInView(ref, { margin: "-35px" });
+  const isInView = useInView(ref, { margin: "-15px" });
 
   return (
     <section id="contato" className="containerContact" ref={ref}>
@@ -14,10 +14,18 @@ function Contact() {
         animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
         transition={{ duration: 0.8 }}
       >
-        <div>
-          <h3>Telefone</h3>
-          <h3>55 77981187169</h3>
+        <div className="containerPhone">
+          <div className="containerTextPhone">
+            <img src={require("../../assets/images/telefone-logo.png")} className="logoPhone"/>
+            <h3 className="textPhoneText">Telefone:</h3>
+          </div>
+          <h3 className="textPhone">55 77981187169</h3>
         </div>
+        <div className="containerEmail">
+          <h3 className="textEmailText">E-mail:</h3>
+          <h3 className="textEmail">guybsonlopescomercial@gmail.com</h3>
+        </div>
+        <h3 className="textCopright">© 2024 Guybson Lopes. Todos os direitos reservados.</h3>
         
       </motion.div>
     </section>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -13,7 +13,7 @@ function Projects() {
     {
       id: '1',
       image: require('../../assets/images/projeto-api-pib.jpg'),
-      name: 'Desenvolvimento de API PIB Países Africanos',
+      name: 'Desenvolvimento API do PIB em Países Africanos',
       link: 'https://github.com/GuybsonDev/API-Analise-PIB-Africano',
       desc: 'Desenvolvi uma API funcional com parâmetros e filtros avançados para aprimorar meu conhecimento em desenvolvimento de APIs. A fonte da API é uma base de dados sobre o crescimento do PIB dos países africanos, cobrindo o período de 1960 a 2023 (63 anos), extraída do Kaggle. Para manipulação da planilha, utilizei a biblioteca Pandas, que possibilitou uma análise eficiente dos dados, e para a construção da API, optei pelo Flask, garantindo uma implementação simples e rápida. A API foi desenvolvida no ambiente de programação Replit, permitindo que ela seja acessada e consumida de qualquer lugar. Essa API oferece duas formas de consumo: é possível acessar todos os dados completos ou fazer consultas filtradas por período anual, utilizando parâmetros para restringir os resultados conforme a necessidade. Para testar o funcionamento da API, utilizei a biblioteca requests em um código separado, permitindo realizar requisições HTTP e garantir que a API está operando corretamente, além de validar o consumo de dados de forma eficiente',
       tecnologycs: [require('../../assets/images/flask-logo.png'),
@@ -143,6 +143,8 @@ function Projects() {
               <img key={index} src={tech} className="imageTecnologycs" />
             ))}
           </div>
+          <hr className="divider" />
+          <a href={data[activeIndex].link} target="_blank" rel="noopener noreferrer" className="linkProject">Acesse o projeto aqui!</a>
         </motion.div>
       </motion.div>
     </section>
